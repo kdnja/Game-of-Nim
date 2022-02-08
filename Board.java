1e68d22efd54d54f99e69c49f7bc7731c6b039a5
Caden Jamason <me@kdn.pw>
2022-02-07 19:39:56 -0800

    Implement maxGuess with corresponding methods

--- Board.java
+++ Board.java
@@ -2,21 +2,29 @@ import java.lang.Math;
 
 public class Board {
     private static int pieces;
-
-    public static int getRandomNumber(int min, int max) {
-        return (int) ((Math.random() * (max - min)) + min);
-    }
+    private static int maxGuess;
 
     public static void populate() {
-        pieces = getRandomNumber(10, 50);
+        pieces = (int) ((Math.random() * (51 - 10)) + 10);
         System.out.println("Number of pieces: " + pieces);
     }
 
-    public static int getNumPieces() {
+    public static int getPieces() {
         return pieces;
+    }
+
+    public static int getMaxGuess() {
+        return maxGuess;
     }
 
     public static void removePieces(int n) {
         pieces -= n;
+    }
+
+    public static void setMaxGuess() {
+        maxGuess = (int) (pieces / 2);
+        if (pieces == 1) {
+            maxGuess = 1;
+        }
     }
 }
